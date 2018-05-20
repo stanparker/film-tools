c3009a73b6667d102afb664af018460fa3e10aa8
= <=>
2018-05-19 18:37:42 -0600

    add hide_xmls and rename scripts to repo

---
+++ rename_with_folder.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+if len(sys.argv) is not 2:
+    print ("Improper number of arguments.")
+
+else:
+    parent = sys.argv[1]
+
+    for root, dirs, files in os.walk(parent):
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        if not files:
+            continue
+
+        prefix = os.path.basename(root)
+
+        for f in files:
+
+            print ("\n \n Renaming:")
+            print ( os.path.join(root, f) + "\n" )
+            print ("To:")
+            print ( os.path.join(root, "{}_{}".format(prefix,f)))
+
+            os.rename(os.path.join(root, f), os.path.join(root, "{}_{}".format(prefix, f)))
